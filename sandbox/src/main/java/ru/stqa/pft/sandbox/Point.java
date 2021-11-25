2d81aca6e7a8ff1ce679c970781d1df2a9f9e646
JuliaCrace <ulagr79@mail.ru>
2021-11-25 09:33:28 +0300

    Вычисление расстояния между двумя точками при помощи метода в классе Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -5,8 +5,19 @@ public class Point {
     public double y;
 
     public Point(double x, double y) {
-     this.x = x;
-     this.y = y;
+        this.x = x;
+        this.y = y;
     }
+     public double distance(Point p2){
+        return Math.sqrt((p2.x - this.x) * (p2.x - this.x) + (p2.y - this.y) * (p2.y - this.y));
+     }
+}
 
+class PointSt {
+    public static void main(String[] args) {
+
+        Point p1 = new Point(1, -7);
+        Point p2 = new Point(-4, 5);
+        System.out.println(p1.distance(p2));
+    }
 }
