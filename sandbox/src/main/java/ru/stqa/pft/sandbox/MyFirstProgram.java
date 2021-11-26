f9dd7a09dd3e013dccbfe4fa57023ce604b935dc
JuliaCrace <ulagr79@mail.ru>
2021-11-26 10:13:17 +0300

    Доработки к заданию 2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,12 +7,7 @@ public class MyFirstProgram {
         Point p1 = new Point(1, -7);
         Point p2 = new Point(-4, 5);
 
-        System.out.println("Расстояние между двумя точками: P1(" + p1.x + "," + p1.y + ")" + " и P2(" + p2.x + "," + p2.y + ")" + " равно " + distance(p1, p2));
-
-    }
-
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+        System.out.println("Расстояние между двумя точками: P1(" + p1.x + "," + p1.y + ")" + " и P2(" + p2.x + "," + p2.y + ")" + " равно " + p1.distance(p2));
 
     }
 
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -12,13 +12,5 @@ public class Point {
     public double distance(Point p2) {
         return Math.sqrt((p2.x - this.x) * (p2.x - this.x) + (p2.y - this.y) * (p2.y - this.y));
     }
-}
-
-class PointSt {
-    public static void main(String[] args) {
-
-        Point p1 = new Point(1, -7);
-        Point p2 = new Point(-4, 5);
-        System.out.println(p1.distance(p2));
-    }
+    
 }
