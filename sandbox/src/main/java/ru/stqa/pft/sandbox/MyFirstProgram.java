c14654c041ce39e21a15069989da912681792b1a
JuliaCrace <ulagr79@mail.ru>
2021-12-05 20:18:08 +0300

    Задание 2 дополнение

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,10 +4,12 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+
         Point p1 = new Point(1, -7);
         Point p2 = new Point(-4, 5);
 
-        System.out.println("Расстояние между двумя точками: P1(" + p1.x + "," + p1.y + ")" + " и P2(" + p2.x + "," + p2.y + ")" + " равно " + p1.distance(p2));
+        System.out.println("Расстояние между двумя точками функцией P1(" + p1.x + "," + p1.y + ")" + " и P2(" + p2.x + "," + p2.y + ")" + " равно: " + p2.distance(p1, p2));
+        System.out.println("Расстояние между двумя точками методом P1(" + p1.x + "," + p1.y + ")" + " и P2(" + p2.x + "," + p2.y + ")" + " равно: " + p1.distance(p2));
 
     }
 
