e84143ba615b0a177c451f9d90b3a1acfaaf2cbf
JuliaCrace <ulagr79@mail.ru>
2022-01-03 13:17:34 +0300

    Поправлена проверка наличия группы при создании контакта.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddNewContactTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddNewContactTests.java
@@ -2,11 +2,17 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class AddNewContactTests extends TestBase {
 
     @Test
     public void testAddNewContact() throws Exception {
+        app.getNavigationHelper().gotoGroupPage();
+        if (!app.getGroupHelper().isTherAGroup()) ;
+        {
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        }
         app.getContactHelper().gotoContactPage();
         app.getContactHelper().createContact(new ContactData("Alex", "Ivanov", "г. Москва, проспект Ленинский, д.2", "+79510990101", "ivanov@mail.ru", "test1"));
 
