b9b03ab6cf291ccbd646b37b00151f4920d2df70
JuliaCrace <ulagr79@mail.ru>
2022-01-11 21:54:38 +0300

    Лекция 4.5. Формирование коллекции

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -5,15 +5,19 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
+        List<GroupData> before = app.getGroupHelper().getGroupList(); // список объектов GroupData до создания группы
+        //int before = app.getGroupHelper().getGroupCount();
         app.getGroupHelper().createGroup(new GroupData("test1", null, null));
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before + 1);
+        List<GroupData> after = app.getGroupHelper().getGroupList(); // содержит список элементов, после того, как создана новая группа
+       // int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after.size(), before.size() + 1); // проверка размера списка после создания группы с размером списка до создания группы
     }
 
 }
