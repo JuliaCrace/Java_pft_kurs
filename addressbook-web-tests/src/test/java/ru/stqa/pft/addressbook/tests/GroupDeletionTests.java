310f78d91f346685c4eab16a513bb1e7dc0864fd
JuliaCrace <ulagr79@mail.ru>
2022-01-11 21:04:59 +0300

    Лекция 4.3. Количество элементов в коллекции

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -13,9 +14,12 @@ public class GroupDeletionTests extends TestBase {
         {
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }
+        int before = app.getGroupHelper().getGroupCount();
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before - 1);
     }
 
 }
